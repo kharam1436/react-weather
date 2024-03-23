950366d31defc6e804f2cb3dcac44527b0492f64
Haram Kwon <kharam1436@icloud.com>
2024-03-23 04:03:32 -0700

    clean unused variable

--- src/utils/weather.ts
+++ src/utils/weather.ts
@@ -1,6 +1,5 @@
 import { Location, Weather, weatherFunctionName } from './common'
 
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 export const getWeatherByLocation = async function (
   location: Location
 ): Promise<Weather> {
